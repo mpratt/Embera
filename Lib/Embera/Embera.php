81d28b3cbf31b6b3bc7c166fa7c347d8a0b50e1f
Michael Pratt <pratt@hablarmierda.net>
2013-09-23 18:08:20 -0500

    Retagged the 1.0 release - Im dumb! I tagged the release as 1.0, but the Versions constant still had 0.9 .. boooo :( - Added useless badges to the README.

--- Lib/Embera/Embera.php
+++ Lib/Embera/Embera.php
@@ -18,7 +18,7 @@ namespace Embera;
 class Embera
 {
     /** @var int Class constant with the current Version of this library */
-    const VERSION = '0.9';
+    const VERSION = '1.0';
 
     /** @var object Instance of \Embera\Oembed */
     protected $oembed;
