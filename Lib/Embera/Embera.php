d12ca511dec0d08f33072b9ae59a9c0199c5602e
Michael Pratt <pratt@hablarmierda.net>
2015-01-31 20:37:10 -0500

    Tagged new version 1.8.5

--- Lib/Embera/Embera.php
+++ Lib/Embera/Embera.php
@@ -18,7 +18,7 @@ namespace Embera;
 class Embera
 {
     /** @var int Class constant with the current Version of this library */
-    const VERSION = '1.8.4';
+    const VERSION = '1.8.5';
 
     /** @var object Instance of \Embera\Oembed */
     protected $oembed;
