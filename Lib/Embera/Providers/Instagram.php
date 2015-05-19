b2577649152aff31269ad8e0f374bcbadce42fcd
Michael Pratt <pratt@hablarmierda.net>
2015-05-19 12:20:38 -0500

    The instagram::modifyResponse() now check if the `response['url']` exists, before extracting the url extension.

--- Lib/Embera/Providers/Instagram.php
+++ Lib/Embera/Providers/Instagram.php
@@ -25,20 +25,20 @@ class Instagram extends \Embera\Adapters\Service
     protected function validateUrl()
     {
         $this->url->stripQueryString();
-
         return (preg_match('~/p/([\w\d]+)/?$~i', $this->url));
     }
 
     /** inline {@inheritdoc} */
     protected function modifyResponse(array $response = array())
     {
-        $extension = strtolower(pathinfo(parse_url($response['url'],PHP_URL_PATH),PATHINFO_EXTENSION));
-        if (empty($response['html']) && in_array($extension, array('jpg', 'jpeg', 'png', 'gif'))) {
-            $html  = '<a href="' . $response['url'] . '" target="_blank">';
-            $html .= '<img class="instagram-oembed" src="' . $response['url'] . '" width="' . $response['width'] . '" height="' . $response['height'] . '" alt="' . htmlspecialchars($response['title'], ENT_QUOTES, 'UTF-8') . '" title="' . htmlspecialchars($response['title'], ENT_QUOTES, 'UTF-8') . '">';
-            $html .= '</a>';
-
-            $response['html'] = $html;
+        if (empty($response['html']) && !empty($response['url'])) {
+            $extension = strtolower(pathinfo(parse_url($response['url'],PHP_URL_PATH),PATHINFO_EXTENSION));
+            if (in_array($extension, array('jpg', 'jpeg', 'png', 'gif'))) {
+                $html  = '<a href="' . $response['url'] . '" target="_blank">';
+                $html .= '<img class="instagram-oembed" src="' . $response['url'] . '" width="' . $response['width'] . '" height="' . $response['height'] . '" alt="' . htmlspecialchars($response['title'], ENT_QUOTES, 'UTF-8') . '" title="' . htmlspecialchars($response['title'], ENT_QUOTES, 'UTF-8') . '">';
+                $html .= '</a>';
+                $response['html'] = $html;
+            }
         }
 
         return $response;
