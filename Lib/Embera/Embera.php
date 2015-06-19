4ce99a9844e1d7e87f4317e66560ff4040395b0b
Michael Pratt <pratt@hablarmierda.net>
2015-06-19 16:41:44 -0500

    Tagged release as 1.8.10

--- Lib/Embera/Embera.php
+++ Lib/Embera/Embera.php
@@ -18,7 +18,7 @@ namespace Embera;
 class Embera
 {
     /** @var int Class constant with the current Version of this library */
-    const VERSION = '1.8.9';
+    const VERSION = '1.8.10';
 
     /** @var object Instance of \Embera\Oembed */
     protected $oembed;
