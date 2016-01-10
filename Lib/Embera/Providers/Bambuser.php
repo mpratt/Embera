a5c6b78b017ed1eed2730d00d369d9ad6aab3f12
Michael Pratt <pratt@hablarmierda.net>
2016-01-10 07:38:15 -0500

    Updated Bambuser fake response

--- Lib/Embera/Providers/Bambuser.php
+++ Lib/Embera/Providers/Bambuser.php
@@ -50,12 +50,13 @@ class Bambuser extends \Embera\Adapters\Service
             'provider_url' => 'http://bambuser.com',
         );
 
-        $html = '<object id="bplayer" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="{width}" height="{height}">';
-        $html .= '<embed name="bplayer" src="http://static.bambuser.com/r/player.swf?{query}&amp;context=oembed" type="application/x-shockwave-flash" width="{width}" height="{height}" allowfullscreen="true" allowscriptaccess="always" wmode="opaque"></embed>';
-        $html .= '<param name="movie" value="http://static.bambuser.com/r/player.swf?{query}&amp;context=oembed"></param>';
-        $html .= '<param name="allowfullscreen" value="true"></param>';
-        $html .= '<param name="allowscriptaccess" value="always"></param><param name="wmode" value="opaque"></param>';
-        $html .='</object>';
+        $html  = '<object id="bplayer" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="{width}" height="{height}">';
+        $html .= '<embed  allowfullscreen="true" allowscriptaccess="always" wmode="transparent" type="application/x-shockwave-flash" name="bplayer" src="https://static.bambuser.com/r/player.swf?{query}&amp;context=oembed" width="{width}" height="{height}" />';
+        $html .= '<param name="movie" value="https://static.bambuser.com/r/player.swf?context=oembed&amp;{query}" />';
+        $html .= '<param name="allowfullscreen" value="true" />';
+        $html .= '<param name="allowscriptaccess" value="always" />';
+        $html .= '<param name="wmode" value="transparent" />';
+        $html .= '</object>';
 
         if (preg_match('~/v/([0-9]+)$~i', $this->url, $matches)) {
             return array_merge(
