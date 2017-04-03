84df7a11426c017f1aae493a5994babddffac571
Michael Pratt <commits@michael-pratt.com>
2017-04-02 23:52:31 -0500

    Try to fix Travis PHP 7.0 build failing because of: - PHP Fatal error:  Class 'PHPUnit_Framework_TestCase' not found

--- Tests/Autoload.php
+++ Tests/Autoload.php
@@ -9,6 +9,13 @@ else
     require __DIR__ . '/../Lib/Embera/Autoload.php';
 
 /**
+ * Backward Compatability for newer PHPUnit versions (Travis 7.0 and 7.1)
+ */
+if (!class_exists('\PHPUnit\Framework\TestCase') && class_exists('PHPUnit_Framework_TestCase')) {
+    class_alias('PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+}
+
+/**
  * HttpRequest Mockup
  * @codeCoverageIgnore
  */
