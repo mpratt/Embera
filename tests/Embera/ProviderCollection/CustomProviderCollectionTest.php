e65b1f91f933d0ce18bdec16050ca384d97fcd69
Michael Pratt <commits@michael-pratt.com>
2019-12-10 11:17:25 -0500

    Added Custom and Slim Provider Collections

---
+++ tests/Embera/ProviderCollection/CustomProviderCollectionTest.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * CustomProviderCollectionTest.php
+ *
+ * @package Embera
+ * @author Michael Pratt <yo@michael-pratt.com>
+ * @link   http://www.michael-pratt.com/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Embera\ProviderCollection;
+
+use PHPUnit\Framework\TestCase;
+use Embera\Embera;
+
+final class CustomProviderCollectionTest extends TestCase
+{
+    protected $urls = [
+        'http://www.youtube.com/watch?v=WtPiGYsllos&index=1',
+        'https://host.com/invalid/url',
+        'https://www.flickr.com/photos/22134962@N03/8738306577/in/explore-2013-05-14',
+    ];
+
+    protected $config = [
+        'https_only' => false,
+        'fake_responses' => Embera::ALLOW_FAKE_RESPONSES,
+        'maxwidth' => 430,
+        'maxheight' => 270,
+    ];
+
+    public function testCanFindProviders()
+    {
+        $collection = new CustomProviderCollection($this->config);
+        $collection->registerProvider('Youtube');
+        $collection->registerProvider('Flickr');
+
+        $providers = $collection->findProviders($this->urls);
+
+        foreach ($providers as $p) {
+            $this->assertTrue(in_array($p->getProviderName(), [ 'Youtube', 'Flickr' ]));
+        }
+
+        $collection = new CustomProviderCollection($this->config);
+        $collection->registerProvider('Flickr');
+
+        $providers = $collection->findProviders($this->urls);
+
+        foreach ($providers as $p) {
+            $this->assertTrue(in_array($p->getProviderName(), [ 'Flickr' ]));
+        }
+    }
+
+}
---
+++ tests/Embera/ProviderCollection/SlimProviderCollectionTest.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * SlimProviderCollectionTest.php
+ *
+ * @package Embera
+ * @author Michael Pratt <yo@michael-pratt.com>
+ * @link   http://www.michael-pratt.com/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Embera\ProviderCollection;
+
+use PHPUnit\Framework\TestCase;
+use Embera\Embera;
+
+final class SlimProviderCollectionTest extends TestCase
+{
+    protected $urls = [
+        'https://www.kickstarter.com/projects/wilderwoventarot/the-wilderwoven-tarot',
+        'http://www.youtube.com/watch?v=WtPiGYsllos&index=1',
+        'https://repl.it/repls/GloriousMotherlyButton',
+    ];
+
+    protected $config = [
+        'https_only' => false,
+        'fake_responses' => Embera::ALLOW_FAKE_RESPONSES,
+        'maxwidth' => 430,
+        'maxheight' => 270,
+    ];
+
+    public function testCanFindProviders()
+    {
+        $collection = new SlimProviderCollection($this->config);
+        $providers = $collection->findProviders($this->urls);
+
+        foreach ($providers as $p) {
+            $this->assertTrue(in_array($p->getProviderName(), [ 'Youtube', 'Kickstarter' ]));
+        }
+    }
+
+}
