5294f4152e4bb995e8e69ab381f772136e596a2f
Michael Pratt <commits@michael-pratt.com>
2020-01-02 19:34:45 -0500

    Updated Instagram provider

--- Lib/Embera/Providers/Instagram.php
+++ Lib/Embera/Providers/Instagram.php
@@ -25,22 +25,8 @@ class Instagram extends \Embera\Adapters\Service
     protected function validateUrl()
     {
         $this->url->stripQueryString();
-        return (preg_match('~/p/([A-Za-z0-9\-_]+)/?$~i', $this->url));
-    }
-
-    /** inline {@inheritdoc} */
-    protected function modifyResponse(array $response = array())
-    {
-        if (empty($response['html']) && !empty($response['url'])) {
-            $extension = strtolower(pathinfo(parse_url($response['url'],PHP_URL_PATH),PATHINFO_EXTENSION));
-            if (in_array($extension, array('jpg', 'jpeg', 'png', 'gif'))) {
-                $html  = '<a href="' . $response['url'] . '" target="_blank">';
-                $html .= '<img class="instagram-oembed" src="' . $response['url'] . '" width="' . $response['width'] . '" height="' . $response['height'] . '" alt="' . htmlspecialchars($response['title'], ENT_QUOTES, 'UTF-8') . '" title="' . htmlspecialchars($response['title'], ENT_QUOTES, 'UTF-8') . '">';
-                $html .= '</a>';
-                $response['html'] = $html;
-            }
-        }
-
-        return $response;
+        return (
+            preg_match('~/(?:p|tv)/([^/]+)/?$~i', $this->url) || preg_match('~/([^/]+)/(?:p|tv)/([^/]+)/?$~i', $this->url)
+        );
     }
 }
