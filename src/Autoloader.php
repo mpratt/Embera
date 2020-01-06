ecdf8112a539a32a4dcdb2a7b977e743dd9ecf95
Vladimir Khramtsov <vkhramtsov@gmail.com>
2020-01-06 15:26:30 +0300

    Improve autoloader and boostrap

--- src/Autoloader.php
+++ src/Autoloader.php
@@ -1,7 +1,7 @@
 <?php
 spl_autoload_register(static function ($class) {
-    if (!preg_match('~Embera~i', $class)) {
-        return ;
+    if (false === stripos($class, 'Embera')) {
+        return;
     }
 
     $file = __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
