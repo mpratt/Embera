7bc5097a883f925aaeaa435efd118d402233313e
Michael Pratt <commits@michael-pratt.com>
2020-05-12 19:23:13 -0500

    Tagged version 2.0.7

--- src/Embera/Embera.php
+++ src/Embera/Embera.php
@@ -27,7 +27,7 @@ use Embera\ProviderCollection\DefaultProviderCollection;
 class Embera
 {
     /** @var string Current Library Version */
-    const VERSION = '2.0.6';
+    const VERSION = '2.0.7';
 
     /**
      * Constants describing how the library is
