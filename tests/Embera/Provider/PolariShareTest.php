74c778bbaf5d58af43a020242e2348d90c509e6b
Michael Pratt <commits@michael-pratt.com>
2020-11-07 19:22:05 -0500

    Updated PolariShare test urls

--- tests/Embera/Provider/PolariShareTest.php
+++ tests/Embera/Provider/PolariShareTest.php
@@ -21,8 +21,6 @@ final class PolariShareTest extends ProviderTester
 {
     protected $tasks = [
         'valid_urls' => [
-            'http://www.polarishare.com/@Chris%20Lee/sales-and-marketing-alignment-the-good-the-bad-and-the-ugly-0xtogp/1-SALES-MA/',
-            'https://www.polarishare.com/@Chris%20Lee/dive-into-deep-learning-kor-p808gk/1-Dive-into',
             'https://polarishare.com/@jay2/functional-programming-in-javascript-chapter-4-s54sg1/1-Functional',
         ],
         'invalid_urls' => [
