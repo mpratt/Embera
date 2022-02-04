8609f62ea7492351088bfd44f593baef4342b11b
Michael Pratt <commits@michael-pratt.com>
2022-02-04 14:31:50 -0500

    Added missing methods to the CacheAdapter in order to comply with the interfase.

--- src/Embera/Cache/CacheAdapter.php
+++ src/Embera/Cache/CacheAdapter.php
@@ -74,4 +74,46 @@ abstract class CacheAdapter
 
         return true;
     }
+
+    /**
+     * Fetches a value from the cache.
+     *
+     * @param string $key     The unique key of this item in the cache.
+     * @param mixed  $default Default value to return if the key does not exist.
+     * @return mixed The value of the item from the cache, or $default in case of cache miss.
+     *
+     * @throws InvalidArgumentException
+     */
+    public function get($key, $default = null)
+    {
+        return true;
+    }
+
+    /**
+     * Set data in cache.
+     *
+     * @param string                 $key   The key of the item to store.
+     * @param mixed                  $value The value of the item to store. Must be serializable.
+     * @param null|int|\DateInterval $ttl   Optional. The TTL value of this item. If no value is sent and
+     * @return bool
+     *
+     * @throws InvalidArgumentException
+     */
+    public function set($key, $value, $ttl = null)
+    {
+        return true;
+    }
+
+    /**
+     * Delete an item from the cache by its unique key.
+     *
+     * @param string $key The unique cache key of the item to delete.
+     * @return bool
+     *
+     * @throws InvalidArgumentException
+     */
+    public function delete($key)
+    {
+        return true;
+    }
 }
