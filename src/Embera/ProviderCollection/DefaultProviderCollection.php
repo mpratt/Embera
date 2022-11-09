8dce4ba54b725a31015add6626905f1c6cfdad5e
Michael Pratt <commits@michael-pratt.com>
2022-11-09 17:38:46 -0500

    Removed LillePod. Oembed api seems to be shutdown.

--- src/Embera/ProviderCollection/DefaultProviderCollection.php
+++ src/Embera/ProviderCollection/DefaultProviderCollection.php
@@ -131,7 +131,6 @@ class DefaultProviderCollection extends ProviderCollectionAdapter
             'Knacki',
             'LearningApps',
             'Libsyn',
-            'LillePod',
             'Lineplace',
             'Livestream',
             'LocalVoicesNetwork',
