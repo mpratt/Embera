9cea6baad6a1011e1940ac6362609e93903b8278
Michael Pratt <commits@michael-pratt.com>
2022-11-10 08:45:58 -0500

    Removed ZnipeTV. Site has been inactive for a long time.

--- src/Embera/ProviderCollection/DefaultProviderCollection.php
+++ src/Embera/ProviderCollection/DefaultProviderCollection.php
@@ -254,7 +254,6 @@ class DefaultProviderCollection extends ProviderCollectionAdapter
             'Youtube',
             'Zeplin',
             'Zingsoft',
-            'ZnipeTV',
             'Zoomable',
         ]);
     }
