fc60856a83a8d766b8ff0fe5f1b899fe604bc2c2
Karl Johann Schubert <kajo@kjschubert.de>
2024-02-28 11:58:20 +0100

    Fix: Spotify oEmbed regex (add artist and intl)

--- tests/Embera/Provider/SpotifyTest.php
+++ tests/Embera/Provider/SpotifyTest.php
@@ -24,6 +24,8 @@ final class SpotifyTest extends ProviderTester
             'https://open.spotify.com/album/4FCuyUjOkT28PnFo6A6vkf/4K4sc36DCTp9YJNVu5zV09',
             'https://open.spotify.com/track/0Wm3w3YiMe7YiS8erpKbOl',
             'https://open.spotify.com/show/3wBfqov60qDZbEVjPHo0a8?si=2ee16a763fd44148',
+            'https://open.spotify.com/intl-de/artist/5eAWCfyUhZtHHtBdNk56l1?si=i0-JP01CRz-X4n_pQOZpKA',
+            'https://open.spotify.com/artist/5eAWCfyUhZtHHtBdNk56l1?si=i0-JP01CRz-X4n_pQOZpKA'
         ],
         'invalid_urls' => [
             'https://open.spotify.com/stuff/3zIzQMHvOlw3ukDhRKR2jO',
