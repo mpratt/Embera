069305b9252a428ba4ae6eb58d3dc8c6d9be5cd0
Michael Pratt <commits@michael-pratt.com>
2024-08-16 23:27:29 -0500

    Released Version 2.0.41

--- src/Embera/Embera.php
+++ src/Embera/Embera.php
@@ -27,7 +27,7 @@ use Embera\ProviderCollection\DefaultProviderCollection;
 class Embera
 {
     /** @var string Current Library Version */
-    const VERSION = '2.0.40';
+    const VERSION = '2.0.41';
 
     /**
      * Constants describing how the library is
