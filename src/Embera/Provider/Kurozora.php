7a34750b31949463f8eccbabcd877a7c16c69d84
Michael Pratt <commits@michael-pratt.com>
2025-01-04 00:32:01 -0500

    feat(Kurozora): It now has responsive responses

--- src/Embera/Provider/Kurozora.php
+++ src/Embera/Provider/Kurozora.php
@@ -36,7 +36,7 @@ class Kurozora extends ProviderAdapter implements ProviderInterface
     protected $httpsSupport = true;
 
     /** inline {@inheritdoc} */
-    protected $responsiveSupport = false;
+    protected $responsiveSupport = true;
 
     /** inline {@inheritdoc} */
     public function validateUrl(Url $url)
